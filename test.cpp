173fad1692012173595c3c8ed58c9f869d2625d4
qwed86 <www.120944258@gmail.com>
2025-08-28 11:05:41 +0800

    Cpptest

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+using namespace std;
+using namespace chrono;
+
+int main() {
+    const int N = 100000000; // 1亿
+    vector<int> arr(N, 1);   // 初始化数组全是1
+
+    auto start = high_resolution_clock::now();
+
+    long long sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += arr[i];
+    }
+
+    auto end = high_resolution_clock::now();
+    cout << "sum = " << sum << endl;
+    cout << "耗时: " 
+         << duration_cast<milliseconds>(end - start).count() 
+         << " ms" << endl;
+
+    return 0;
+}
